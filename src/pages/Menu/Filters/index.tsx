101710e4e1692ebc8e39cb891325d36f4eb15777
Rafael Maldonado <nuttirafael@gmail.com>
2022-03-22 16:12:45 -0300

    v0.1.3

--- src/pages/Menu/Filters/index.tsx
+++ src/pages/Menu/Filters/index.tsx
@@ -1,14 +1,22 @@
+import React from 'react';
 import filters from './filters.json';
 import styles from './Filters.module.scss';
 
 type IOption = typeof filters[0];
 
-export default function Filters() {
-    function selectFilter(option: IOption) {}
+interface Props {
+    filter: number | null;
+    setFilter: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export default function Filters({filter, setFilter}: Props) {
+    function selectFilter(option: IOption) {
+        return setFilter(option.id);
+    }
     return ( 
     <div className={styles.filters}>
         {filters.map((option) => (
-            <button className={styles.filters__filter} key={option.id} onClick={() => selectFilter(option)}>
+            <button className={`${styles.filters__filter} ${filter === option.id ? styles["filters__filter--actived"] : ""}`} key={option.id} onClick={() => selectFilter(option)}>
                 {option.label}
             </button>
         ))}
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -7,6 +7,7 @@ import Filters from './Filters';
 
 export default function Menu() {
     const [search, setSearch] = useState("");
+    const [filter, setFilter] = useState<number | null>(null);
     return (
         <main>
             <nav className={styles.menu}>
@@ -19,7 +20,7 @@ export default function Menu() {
                 <h3 className={styles.options__title}>Menu</h3>
                 <Search search={search} setSearch={setSearch} />
                 <div className={styles.options__filters}>
-                    <Filters />
+                    <Filters filter={filter} setFilter={setFilter}/>
                 </div>
             </section>
         </main>
