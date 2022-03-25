f0aad3e95c7936b17064d5e176a00978d7562d45
rafaelnmaldonado <rafaelnuttimaldonado@gmail.com>
2022-03-25 11:27:09 -0300

    v0.1.7

--- src/pages/Menu/Select/index.tsx
+++ src/pages/Menu/Select/index.tsx
@@ -2,12 +2,18 @@ import styles from './Select.module.scss';
 import option from './options.json';
 import {useState} from "react";
 import classNames from "classnames";
+import {MdKeyboardArrowUp, MdKeyboardArrowDown} from 'react-icons/md';
 
 export default function Select () {
     const [open, setOpen] = useState(false);
     return (
-      <button className={styles.select} onClick={() => setOpen(!open)}>
+      <button
+        className={styles.select}
+        onClick={() => setOpen(!open)}
+        onBlur={() => setOpen(false)}
+      >
             <span>Order by</span>
+            {open ? <MdKeyboardArrowUp size={22} /> : <MdKeyboardArrowDown size={22} />}
             <div className={classNames({
               [styles.select__options]: true,
               [styles["select__options--active"]]: open
